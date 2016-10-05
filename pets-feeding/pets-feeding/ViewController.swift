bd226f7f2fcf70681cae39105271eeea9053c956
Nikolay Dementiev <nikolas.dementiev@gmail.com>
2016-10-05 17:58:12 +0300

    initial new project

---
+++ pets-feeding/pets-feeding/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pets-feeding
+//
+//  Created by Nikolay Dementiev on 05.10.16.
+//  Copyright © 2016 mc373. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
---
+++ pets-feeding/pets-feedingTests/pets_feedingTests.swift
@@ -0,0 +1,36 @@
+//
+//  pets_feedingTests.swift
+//  pets-feedingTests
+//
+//  Created by Nikolay Dementiev on 05.10.16.
+//  Copyright © 2016 mc373. All rights reserved.
+//
+
+import XCTest
+@testable import pets_feeding
+
+class pets_feedingTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
