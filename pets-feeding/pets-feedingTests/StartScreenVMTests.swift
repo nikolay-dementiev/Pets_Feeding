455b5b6ad88c5137dff085bb99a6608fa8d190f1
Dementiev Nikolay <dementievnikolay@gmail.com>
2016-10-18 13:57:19 +0300

    tests on StartScreenVM. DONE. TG-17 #done

--- pets-feeding/pets-feedingTests/StartScreenVMTests.swift
+++ pets-feeding/pets-feedingTests/StartScreenVMTests.swift
@@ -25,148 +25,193 @@ class StartScreenVMTests: XCTestCase {
 		super.tearDown()
 	}
 
-	func testChesckStausCalendar() {
-		let tempParam = GetCalendarChekStatusInterlayer()//StartScreenVM.calendarChekStatusInterlayer.self
+	func testsGettingCalendarStatusInterlayer() {
+
+		let tempParam = GetCalendarChekStatusInterlayer()
 
 		var testStatus:EKAuthorizationStatus
 		var expectedValue:StartScreenVM.calendarChekStatusInterlayer
 
+		//1
 		testStatus = EKAuthorizationStatus.authorized
 		expectedValue = tempParam.accessGranted
 
+		testsClassExample.gettingCalendarStatusInterlayer(testStatus, completion: { returnedValue in
+			XCTAssertEqual(returnedValue, expectedValue, "gettingCalendarStatusInterlayer() ERROR!, tested status: \(testStatus), result expected: \(expectedValue), returned from func.: \(returnedValue)") })
 
-		testsClassExample.gettingCalendarStatusInterlayer(testStatus, completion: { status in
-			XCTAssertEqual(status, expectedValue, "gettingCalendarStatusInterlayer() ERROR!, tested status: \(testStatus), result expected: \(expectedValue), returned from func.: \(status)")
-		})
 
+		//2
+		testStatus = EKAuthorizationStatus.denied
+		expectedValue = tempParam.accessDenied
+
+		testsClassExample.gettingCalendarStatusInterlayer(testStatus, completion: { returnedValue in
+			XCTAssertEqual(returnedValue, expectedValue, "gettingCalendarStatusInterlayer() ERROR!, tested status: \(testStatus), result expected: \(expectedValue), returned from func.: \(returnedValue)") })
+
+		//3
+		testStatus = EKAuthorizationStatus.notDetermined
+		if let statusER2 = chekRequestAccess() {
+
+			if statusER2 == true {
+				expectedValue = tempParam.accessGranted
+			} else {
+				expectedValue = tempParam.accessDenied
+			}
+			testsClassExample.gettingCalendarStatusInterlayer(testStatus, completion: { returnedValue in
+				XCTAssertEqual(returnedValue, expectedValue, "gettingCalendarStatusInterlayer() ERROR!, tested status: \(testStatus), result expected: \(expectedValue), returned from func.: \(returnedValue)") })
+		}
 	}
 
+		func testTestAccessToCalendar() {
+	
+			let tempParam = GetCalendarChekStatusInterlayer()
+	
+			//1
+			XCTAssertNotNil(chekRequestAccess(), "RequestAccessStatus can't be 'nil' !")
+	
+			//2
+			var testStatus = true
+	
+			let expectedValue2 = tempParam.accessGranted
+			let returnedValue2 = testsClassExample.testAccessToCalendar(access: testStatus)
+			XCTAssertEqual(returnedValue2, expectedValue2, "testAccessToCalendar() ERROR!, tested status: \(testStatus), result expected: \(expectedValue2), returned from func.: \(returnedValue2)")
+	
+			//2.1
+			testStatus = false
+	
+			let expectedValue2_1 = tempParam.accessDenied
+			let returnedValue2_1 = testsClassExample.testAccessToCalendar(access: testStatus)
+			XCTAssertEqual(returnedValue2_1, expectedValue2_1, "testAccessToCalendar() ERROR!, tested status: \(testStatus), result expected: \(expectedValue2_1), returned from func.: \(returnedValue2_1)")
+	
+		}
 
-//	func testGettingCalendarStatusInterlayer() {
-//
-//		let tempParam = GetCalendarChekStatusInterlayer()//StartScreenVM.calendarChekStatusInterlayer.self
-//
-//		var testStatus:EKAuthorizationStatus
-//		var expectedValue:StartScreenVM.calendarChekStatusInterlayer
-//		var returnedValue:StartScreenVM.calendarChekStatusInterlayer
-//
-//		//1 accessGranted
-//		testStatus = EKAuthorizationStatus.authorized
-//		expectedValue = tempParam.accessGranted
-//		returnedValue = testsClassExample.gettingCalendarStatusInterlayer(testStatus)
-//		XCTAssertEqual(returnedValue, expectedValue, "gettingCalendarStatusInterlayer() ERROR!, tested status: \(testStatus), result expected: \(expectedValue), returned from func.: \(returnedValue)")
-//
-//		//2 accessDenied
-//		testStatus = EKAuthorizationStatus.denied
-//		expectedValue = tempParam.accessDenied
-//		returnedValue = testsClassExample.gettingCalendarStatusInterlayer(testStatus)
-//		XCTAssertEqual(returnedValue, expectedValue, "gettingCalendarStatusInterlayer() ERROR!, tested status: \(testStatus), result expected: \(expectedValue), returned from func.: \(returnedValue)")
-//
-//		//3 notDetermined - chek current status
-//		testStatus = EKAuthorizationStatus.notDetermined
-//
-//		if let statusER2 = chekRequestAccess() {
-//
-//			if statusER2 == true {
-//				expectedValue = tempParam.accessGranted
-//			} else {
-//				expectedValue = tempParam.accessDenied
-//			}
-//
-//			returnedValue = testsClassExample.gettingCalendarStatusInterlayer(testStatus)
-//
-//			XCTAssertEqual(returnedValue, expectedValue, "gettingCalendarStatusInterlayer() ERROR!, tested status: \(testStatus), result expected: \(expectedValue), returned from func.: \(returnedValue)")
-//
-//		}
-//	}
-//
-//	func testRequestAccessToCalendar() {
-//		let tempParam = GetCalendarChekStatusInterlayer()//StartScreenVM.calendarChekStatusInterlayer.self
-//
-//		//1
-//
-//		let statusRequestAccess:Bool = chekRequestAccess()!
-//		let expectedValue1 = testsClassExample.testAccessToCalendar(access: statusRequestAccess)
-//		
-//		let returnedValue1 = testsClassExample.requestAccessToCalendar(tempParam.accessUncertain)
-//		XCTAssertEqual(returnedValue1, expectedValue1, "requestAccessToCalendar() ERROR!, tested access: \(statusRequestAccess), result expected: \(expectedValue1), returned from func.: \(returnedValue1)")
-//
-//	}
-//
-//	func testTestAccessToCalendar() {
-//
-//		let tempParam = GetCalendarChekStatusInterlayer()
-//
-//		//1
-//		XCTAssertNotNil(chekRequestAccess(), "RequestAccessStatus can't be 'nil' !")
-//
-//		//2
-//		var testStatus = true
-//
-//		let expectedValue2 = tempParam.accessGranted
-//		let returnedValue2 = testsClassExample.testAccessToCalendar(access: testStatus)
-//		XCTAssertEqual(returnedValue2, expectedValue2, "testAccessToCalendar() ERROR!, tested status: \(testStatus), result expected: \(expectedValue2), returned from func.: \(returnedValue2)")
-//
-//		//2.1
-//		testStatus = false
-//
-//		let expectedValue2_1 = tempParam.accessDenied
-//		let returnedValue2_1 = testsClassExample.testAccessToCalendar(access: testStatus)
-//		XCTAssertEqual(returnedValue2_1, expectedValue2_1, "testAccessToCalendar() ERROR!, tested status: \(testStatus), result expected: \(expectedValue2_1), returned from func.: \(returnedValue2_1)")
-//
-//	}
-//
-//	func testGettingStatusFromCalendar() {
-//
-//		let expectedEventValue = getExpectedEventValue()
-//		let returnedEventValue = testsClassExample.gettingStatusFromCalendar()
-//		XCTAssertEqual(returnedEventValue, expectedEventValue, "gettingStatusFromCalendar() ERROR! result expected: \(expectedEventValue), returned from func.: \(returnedEventValue)")
-//
-//	}
-//
-//	func testGetCalendarStatus() {
-//
-//		let expectedEventValue = getExpectedEventValue()
-//		let expectedValue = testsClassExample.gettingCalendarStatusInterlayer(expectedEventValue)
-//		let returnedValue = testsClassExample.getCalendarStatus()
-//		XCTAssertEqual(returnedValue, expectedValue, "getCalendarStatus() ERROR! expected: \(expectedValue), returned from func.: \(returnedValue)")
-//	}
-//
-//
-//
-//	//MARK: - func of class module
-//	private func getExpectedEventValue() -> EKAuthorizationStatus {
-//		return  EKEventStore.authorizationStatus(for: EKEntityType.event)
-//	}
-//
+
+
+	//	func testGettingCalendarStatusInterlayer() {
+	//
+	//		let tempParam = GetCalendarChekStatusInterlayer()//StartScreenVM.calendarChekStatusInterlayer.self
+	//
+	//		var testStatus:EKAuthorizationStatus
+	//		var expectedValue:StartScreenVM.calendarChekStatusInterlayer
+	//		var returnedValue:StartScreenVM.calendarChekStatusInterlayer
+	//
+	//		//1 accessGranted
+	//		testStatus = EKAuthorizationStatus.authorized
+	//		expectedValue = tempParam.accessGranted
+	//		returnedValue = testsClassExample.gettingCalendarStatusInterlayer(testStatus)
+	//		XCTAssertEqual(returnedValue, expectedValue, "gettingCalendarStatusInterlayer() ERROR!, tested status: \(testStatus), result expected: \(expectedValue), returned from func.: \(returnedValue)")
+	//
+	//		//2 accessDenied
+	//		testStatus = EKAuthorizationStatus.denied
+	//		expectedValue = tempParam.accessDenied
+	//		returnedValue = testsClassExample.gettingCalendarStatusInterlayer(testStatus)
+	//		XCTAssertEqual(returnedValue, expectedValue, "gettingCalendarStatusInterlayer() ERROR!, tested status: \(testStatus), result expected: \(expectedValue), returned from func.: \(returnedValue)")
+	//
+	//		//3 notDetermined - chek current status
+	//		testStatus = EKAuthorizationStatus.notDetermined
+	//
+	//		if let statusER2 = chekRequestAccess() {
+	//
+	//			if statusER2 == true {
+	//				expectedValue = tempParam.accessGranted
+	//			} else {
+	//				expectedValue = tempParam.accessDenied
+	//			}
+	//
+	//			returnedValue = testsClassExample.gettingCalendarStatusInterlayer(testStatus)
+	//
+	//			XCTAssertEqual(returnedValue, expectedValue, "gettingCalendarStatusInterlayer() ERROR!, tested status: \(testStatus), result expected: \(expectedValue), returned from func.: \(returnedValue)")
+	//
+	//		}
+	//	}
+	//
+	//	func testRequestAccessToCalendar() {
+	//		let tempParam = GetCalendarChekStatusInterlayer()//StartScreenVM.calendarChekStatusInterlayer.self
+	//
+	//		//1
+	//
+	//		let statusRequestAccess:Bool = chekRequestAccess()!
+	//		let expectedValue1 = testsClassExample.testAccessToCalendar(access: statusRequestAccess)
+	//
+	//		let returnedValue1 = testsClassExample.requestAccessToCalendar(tempParam.accessUncertain)
+	//		XCTAssertEqual(returnedValue1, expectedValue1, "requestAccessToCalendar() ERROR!, tested access: \(statusRequestAccess), result expected: \(expectedValue1), returned from func.: \(returnedValue1)")
+	//
+	//	}
+	//
+	//	func testTestAccessToCalendar() {
+	//
+	//		let tempParam = GetCalendarChekStatusInterlayer()
+	//
+	//		//1
+	//		XCTAssertNotNil(chekRequestAccess(), "RequestAccessStatus can't be 'nil' !")
+	//
+	//		//2
+	//		var testStatus = true
+	//
+	//		let expectedValue2 = tempParam.accessGranted
+	//		let returnedValue2 = testsClassExample.testAccessToCalendar(access: testStatus)
+	//		XCTAssertEqual(returnedValue2, expectedValue2, "testAccessToCalendar() ERROR!, tested status: \(testStatus), result expected: \(expectedValue2), returned from func.: \(returnedValue2)")
+	//
+	//		//2.1
+	//		testStatus = false
+	//
+	//		let expectedValue2_1 = tempParam.accessDenied
+	//		let returnedValue2_1 = testsClassExample.testAccessToCalendar(access: testStatus)
+	//		XCTAssertEqual(returnedValue2_1, expectedValue2_1, "testAccessToCalendar() ERROR!, tested status: \(testStatus), result expected: \(expectedValue2_1), returned from func.: \(returnedValue2_1)")
+	//
+	//	}
+	//
+	//	func testGettingStatusFromCalendar() {
+	//
+	//		let expectedEventValue = getExpectedEventValue()
+	//		let returnedEventValue = testsClassExample.gettingStatusFromCalendar()
+	//		XCTAssertEqual(returnedEventValue, expectedEventValue, "gettingStatusFromCalendar() ERROR! result expected: \(expectedEventValue), returned from func.: \(returnedEventValue)")
+	//
+	//	}
+	//
+	//	func testGetCalendarStatus() {
+	//
+	//		let expectedEventValue = getExpectedEventValue()
+	//		let expectedValue = testsClassExample.gettingCalendarStatusInterlayer(expectedEventValue)
+	//		let returnedValue = testsClassExample.getCalendarStatus()
+	//		XCTAssertEqual(returnedValue, expectedValue, "getCalendarStatus() ERROR! expected: \(expectedValue), returned from func.: \(returnedValue)")
+	//	}
+	//
+	//
+	//
+	//	//MARK: - func of class module
+	//	private func getExpectedEventValue() -> EKAuthorizationStatus {
+	//		return  EKEventStore.authorizationStatus(for: EKEntityType.event)
+	//	}
+	//
 	private func GetCalendarChekStatusInterlayer() -> StartScreenVM.calendarChekStatusInterlayer.Type {
+
 		let tempParam = StartScreenVM.calendarChekStatusInterlayer.self
 		return tempParam
 	}
-//
-//	private func chekRequestAccess() -> Bool? {
-//		var value:Bool?
-//
-//		let evStore = EKEventStore()
-//		evStore.reset()
-//
-//		let expectation1 = expectation(description: "chekRequestAccess() runs the callback closure")
-//
-//		evStore.requestAccess(to: EKEntityType.event, completion: {
-//			(accessGranted: Bool, error: Error?) in
-//			
-//			value = accessGranted
-//
-//			expectation1.fulfill()
-//		})
-//
-//		waitForExpectations(timeout: 5) { error in
-//			if let error = error {
-//				XCTFail("waitForExpectationsWithTimeout errored: \(error)")
-//			}
-//		}
-//
-//		return value
-//	}
 
+	private func chekRequestAccess() -> Bool? {
+		var value:Bool?
+
+		let evStore = EKEventStore()
+		evStore.reset()
+
+		let expectation1 = expectation(description: "chekRequestAccess() runs the callback closure")
+
+		evStore.requestAccess(to: EKEntityType.event, completion: {
+			(accessGranted: Bool, error: Error?) in
+
+			value = accessGranted
+
+			expectation1.fulfill()
+		})
+
+		waitForExpectations(timeout: 5) { error in
+			if let error = error {
+				XCTFail("waitForExpectationsWithTimeout errored: \(error)")
+			}
+		}
+		
+		return value
+	}
+	
 }
